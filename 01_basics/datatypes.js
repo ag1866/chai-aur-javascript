040011f30f1dd1733d7d700b6b00912738dbff42
arpit gupta <113471217+ag1866@users.noreply.github.com>
2024-02-20 18:51:11 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //  this treat all js code as newer version but nowadays this comes by default, we don't need to write
+
+
+// alert( 3 + 3)   we can't use alert in node 
+
+console.log("Arpit Gupta");
+
+
+let name = "Arpit"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined);
+console.log(typeof null);
---
+++ 01_basics/variables.js
@@ -0,0 +1,25 @@
+// here we learn about let var cons variable
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Orai"
+let accountState;
+
+// accountId = 2
+// accountId = 2 is not allowed because constant variable is passed only one time 
+
+
+accountEmail = "hc@hc.com"
+var accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+prefer not to use var
+bacause of issue in block scope and functional scope 
+var ko isliye use nhi krte kyuki agr neeche kuch value change hoti to wo value change krleta h
+jaise ki let aur cons m apn duplicate variable nhi bna skte 
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
