06bc3be80cb39e875dc09bad2fd8ae0a72e10f07
arpit gupta <113471217+ag1866@users.noreply.github.com>
2024-02-20 19:38:17 +0000

    datatypeconversion

---
+++ 01_basics/datatypeconversion.js
@@ -0,0 +1,45 @@
+// let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+// NaN means Not a Number
+
+// let score = null
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+// let score = "abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score) // here we are converting string to number
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+// true => 1; false => 0
+
+// let isLoggedIn = "hitesh"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
