97baa959b93a1009d563f5f81a9a4d1b8102fa9e
arpit gupta <113471217+ag1866@users.noreply.github.com>
2024-02-20 21:21:25 +0000

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,42 @@
+// primitive data types
+
+// 7types : String, Number, Boolean, null, undefined, symbol, BigInt
+
+// javascript is a dynamically typed language
+// because at the time of typing variable type is not defined
+
+
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId =Symbol('123')
+
+console.log(id == anotherId)
+
+// Non-Primitive data types
+
+// arrays, objects, Functions
+
+// arrays
+const heroes = ["shaktiman", "naagraj", "doga"];
+// object
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+// function
+const myFunction = function(){
+    console.log("Hello world")
+}
+
+
+// when we want to know type of any variable
+// console.log(typeof _________); 
+
+
